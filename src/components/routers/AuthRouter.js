bb7405c86145fdcb91dfa563263bcdaafe1dc86a
Abel Guitron <guitronaca@gmail.com>
2022-03-02 17:08:21 -0600

    :lipstick: Adding the register and login styles.

--- src/components/routers/AuthRouter.js
+++ src/components/routers/AuthRouter.js
@@ -5,10 +5,14 @@ import { RegisterScreen } from "../auth/RegisterScreen";
 
 export const AuthRouter = () => {
   return (
-    <Switch>
-      <Route path="/auth/login" component={LoginScreen} />
-      <Route path="/auth/register" component={RegisterScreen} />
-      <Redirect to="/auth/register" />
-    </Switch>
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route path="/auth/login" component={LoginScreen} />
+          <Route path="/auth/register" component={RegisterScreen} />
+          <Redirect to="/auth/register" />
+        </Switch>
+      </div>
+    </div>
   );
 };
