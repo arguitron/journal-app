d18043032feb44bc5f501e997213bfec20715933
Abel Guitron <guitronaca@gmail.com>
2022-03-11 15:59:35 -0600

    :sparkles: Login dispatch.

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,60 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
-    return (
-        <>
-            <h3 className="auth__title">Login</h3>
+  const dispatch = useDispatch();
 
-            <form>
+  const [formValues, handleInputChange] = useForm({
+    email: "nando@gmail.com",
+    password: "12345",
+  });
 
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    className="auth__input"
-                    autoComplete="off"
-                />
+  const { email, password } = formValues;
 
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    className="auth__input"
-                />
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345, "Abelaxo crack"));
+  };
 
+  return (
+    <>
+      <h3 className="auth__title">Login</h3>
 
-                <button
-                    type="submit"
-                    className="btn btn-primary btn-block"
-                >
-                    Login
-                </button>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
 
-                
-                <div className="auth__social-networks">
-                    <p>Login with social networks</p>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+          value={password}
+          onChange={handleInputChange}
+        />
 
-                    <div 
-                        className="google-btn"
-                    >
-                        <div className="google-icon-wrapper">
-                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
-                        </div>
-                        <p className="btn-text">
-                            <b>Sign in with google</b>
-                        </p>
-                    </div>
-                </div>
+        <button type="submit" className="btn btn-primary btn-block">
+          Login
+        </button>
 
-                <Link 
-                    to="/auth/register"
-                    className="link"
-                >
-                    Create new account    
-                </Link>
+        <div className="auth__social-networks">
+          <p>Login with social networks</p>
 
-            </form>
-        </>
-    )
-}
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+
+        <Link to="/auth/register" className="link">
+          Create new account
+        </Link>
+      </form>
+    </>
+  );
+};
